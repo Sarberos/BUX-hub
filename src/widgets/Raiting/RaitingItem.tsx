cd20779a6a6d7aba0c68f9f5795db5a28c1c3487
sarberos <denismasters@mail.ru>
2024-08-25 12:05:18 +0300

    connection raiting page  to api

--- src/widgets/Raiting/RaitingItem.tsx
+++ src/widgets/Raiting/RaitingItem.tsx
@@ -7,17 +7,17 @@ export type TRaitingItemStyle={
     backgroundColor: CSSProperties,
 }
 
-export default function ({active_usernames,coins,place}:TRaitngItem&{place:number}){
-    const meItemStyle: React.CSSProperties = false
+export default function ({active_usernames,coins,place,telegramId}:TRaitngItem&{place:number}){
+    const{user}=useTelegramApi()
+    const meItemStyle: React.CSSProperties = user?.id ===telegramId
       ? { backgroundColor: "#fff", position: "sticky", bottom: 0,}
       : {};  
-    const{user}=useTelegramApi()
-    console.log(user);
-    
+
+    const text_color:string =place===1? '#FFCC48':place===1?'#ACACAC':place===1?'#9E5228':'';
     return(
         <div style={meItemStyle} className={ false  ? `${s.raiting_item_wrap} ${s.active}`: `${s.raiting_item_wrap}`}>
             <div className={s.raiting_info}>
-                <p className={s.place_number}>{place}</p>
+                <p color={text_color} className={s.place_number}>{place}</p>
                 <div className={s.raiting_title_wrap}>
                     <p className={s.raiting_title}>{active_usernames && active_usernames[0]}</p>
                     <p className={s.raiting_subtitle}>{`${3} lvl`}</p>
