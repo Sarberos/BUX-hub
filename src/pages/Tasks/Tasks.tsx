c327907c1e08be1a8d516d1d76e024a2077b4b6a
sarberos <denismasters@mail.ru>
2024-08-29 11:07:02 +0300

    connection to api

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,27 +1,22 @@
 import s from '@pages/Tasks/Tasks.module.scss'
 import { TTaskItem, useGetTasksInf } from '@shared/Tasks/hooks/useGetTasksInf';
 import TaskItem from '@widgets/Tasks/TaskItem/TaskItem'
-import { useAppDispatch, useAppSelector } from '@shared/utilits/redux/hooks';
-import {setIsMiniTasks } from '@shared/utilits/redux/redux_slice/home_slice';
+import {useAppSelector } from '@shared/utilits/redux/hooks';
 import { Preloader } from '@widgets/UI/Preloader/Preloader';
 import { useTranslation } from 'react-i18next';
 import { useClaimTasksCoins } from '@shared/Tasks/hooks/useClaimTasksCoins';
-import PopUp from '@widgets/UI/PopUp/PopUp';
 import {useEffect, useState} from 'react'
 // import { MiniTasks } from '@widgets/Tasks/MiniTasks/MiniTasks';
 
 export const Tasks=()=>{
   const {t}= useTranslation()
-// const queryClient =useQueryClient()
-  const dispatch=useAppDispatch()
   const state = useAppSelector(state=>state.home)
 
   const {data:tasksInf,isLoading:taskInfoLoading}=useGetTasksInf()
   const {mutate:claimTasksCoins}=useClaimTasksCoins()
   
   const [tasksList , setTasksList]=useState<TTaskItem[]>([])
-  const [miniTaskOpen , setminiTaskOpen]=useState<boolean>(false)
-  // const [completedTasks,setCompletedTasks]=useState<TTaskItem[]>([])
+  
   console.log('TASK MINITASK STATUS'+ state.isMiniTasks);
   
 
@@ -51,12 +46,6 @@ if(taskInfoLoading){
   return <Preloader />
 }else 
   return (
-  <>
-   {miniTaskOpen &&  <div className={state.isMiniTasks ?`${s.mini_tasks_wrap} ${s.active}` :`${s.mini_tasks_wrap}`}>
-    <PopUp onClose={()=>dispatch(setIsMiniTasks(false))}>
-      {/* <MiniTasks tasksList={tasksList} completedTasks={completedTasks} onMiniTaskClaim={onMiniTaskClaim}/> */}
-    </PopUp>
-  </div> }
     <div className={s.task_wrapper}>
       <div className={s.title_wrap} >
         <div className={s.title}>{t("tasks")}</div>
@@ -65,7 +54,7 @@ if(taskInfoLoading){
         <div className={s.task_list}>
           {tasksInf?.content.filter(elem=>elem.status!=='claimed').map((elem, index) => 
             <TaskItem
-            setminiTaskOpen={setminiTaskOpen}
+            // setminiTaskOpen={setminiTaskOpen}
             claimTasksCoins={claimTasksCoins}
               {...elem}
               key={index}
@@ -73,7 +62,6 @@ if(taskInfoLoading){
           )}
         </div>
     </div>
-  </>
     );
 
 }
--- src/widgets/Tasks/TaskItem/TaskItem.tsx
+++ src/widgets/Tasks/TaskItem/TaskItem.tsx
@@ -13,7 +13,7 @@ import { useQueryClient } from '@tanstack/react-query'
 
 
 
-export default function({title,sub_tasks,coins,id,link,status,main_task_id,claimTasksCoins,setminiTaskOpen}:TTaskItem&{claimTasksCoins?:(value:number)=>void,setminiTaskOpen?:(value:boolean)=>void}){
+export default function({title,sub_tasks,coins,id,link,status,main_task_id,claimTasksCoins}:TTaskItem&{claimTasksCoins?:(value:number)=>void,setminiTaskOpen?:(value:boolean)=>void}){
     const queryClient = useQueryClient()
     const {t}=useTranslation()
     const dispatch = useAppDispatch()
@@ -34,7 +34,6 @@ export default function({title,sub_tasks,coins,id,link,status,main_task_id,claim
         queryClient.invalidateQueries({queryKey:['task_inf']})
     }
     const handleOpen=(id:number)=>{
-        setminiTaskOpen && setminiTaskOpen(true)
         dispatch(setIsMiniTasks(true))
         dispatch(setMiniTaskId(id))
     }
