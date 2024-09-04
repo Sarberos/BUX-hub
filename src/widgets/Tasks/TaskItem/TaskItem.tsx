f50257d95f37e271d7a5de72b595f1a9512125ce
sarberos <denismasters@mail.ru>
2024-09-04 13:28:39 +0300

    fix bags

--- src/widgets/Tasks/TaskItem/TaskItem.tsx
+++ src/widgets/Tasks/TaskItem/TaskItem.tsx
@@ -54,14 +54,14 @@ export default function({title,sub_tasks,coins,id,link,status,main_task_id,chann
                     <p className={s.item_subtitle}>{sub_tasks && sub_tasks.length!==0 ? `0/${sub_tasks.length} tasks, +${coins} `:`+${coins}`}</p>
                 </div>
             </div>
-            {status==='pending' && <button onClick={()=>{handleStart(id)}} className={s.status_btn}>{t("start")}</button>} 
+            {!sub_tasks && status==='pending' && <button onClick={()=>{handleStart(id)}} className={s.status_btn}>{t("start")}</button>} 
             {sub_tasks && sub_tasks.length !==0 && <button onClick={()=>handleOpen(id)} className={s.status_btn}>{t("open")}</button>}
             {main_task_id!==null && status ==='completed' && 
             <button disabled={true} className={`${s.status_btn} ${s.success}`}>
                 <img src={success_arrow} className={s.success_img}/>
             </button>}
             {sub_tasks && sub_tasks.length==0 && status==='claimed' || sub_tasks && sub_tasks.length==0 && status==='in-progress' && <button disabled={true} className={`${s.status_btn} ${s.disable}`}>{t("Claim")}</button>} 
-            {main_task_id===null && status==='completed' && <button onClick={()=>{handleClaim(id)}} className={`${s.status_btn}`}>{t("Claim")}</button>} 
+            {!sub_tasks && main_task_id===null && status==='completed' && <button onClick={()=>{handleClaim(id)}} className={`${s.status_btn}`}>{t("Claim")}</button>} 
 
         </div>
     )
