83b2e6771b1bd79e634fb679abcf35d5beb04a2d
sarberos <denismasters@mail.ru>
2024-09-05 20:09:06 +0300

    fix anim size

--- src/widgets/Home/AnimationMainImg/AnimationMainImg.tsx
+++ src/widgets/Home/AnimationMainImg/AnimationMainImg.tsx
@@ -6,7 +6,7 @@ export const AnimationMainImg = () => {
     const [size, setSize] = useState(300);  
 
     const updateSize = () => {  
-        const newHeight = window.innerHeight * 0.4; 
+        const newHeight = window.innerHeight * 0.35; 
 
         setSize(newHeight);  
     };  
