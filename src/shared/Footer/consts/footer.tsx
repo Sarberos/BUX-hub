480ae94cdb87833efe7c8995e8caad76415fae02
sarberos <denismasters@mail.ru>
2024-09-13 18:19:00 +0300

    change  to high quality icons

--- src/shared/Footer/consts/footer.tsx
+++ src/shared/Footer/consts/footer.tsx
@@ -1,12 +1,7 @@
-// import home_ico from '@shared/Footer/assets/footer_img/Mask group600px_new_home.svg'
-// import tasks_ico from '@shared/Footer/assets/footer_img/Mask group600px_new_tasks.svg'
-// import raiting_ico from '@shared/Footer/assets/footer_img/Mask group600px_new_raiting.svg'
-// import frens_ico from '@shared/Footer/assets/footer_img/Mask group600px_new_frens.svg'
-// import home_ico from '@shared/Footer/assets/footer_img/Mask group300px_new_a_home.svg'
-import home_ico from '@shared/assets/high quality svg/home_red.svg'
-import tasks_ico from '@shared/assets/high quality svg/tasks_red.svg'
-import raiting_ico from '@shared/assets/high quality svg/raiting.svg';
-import frens_ico from '@shared/assets/high quality svg/frens_red.svg'
+import home_ico from '@shared/assets/high quality svg/home_black_red.svg'
+import tasks_ico from '@shared/assets/high quality svg/tasks_black_red.svg'
+import raiting_ico from '@shared/assets/high quality svg/raiting_black.svg';
+import frens_ico from '@shared/assets/high quality svg/frens_black_red.svg'
 import home_active_ico from '@shared/assets/high quality svg/home_red.svg'
 import tasks_active_ico from '@shared/assets/high quality svg/tasks_red.svg'
 import raiting_active_ico from '@shared/assets/high quality svg/raiting.svg';
