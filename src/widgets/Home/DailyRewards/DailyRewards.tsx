fa331f2d34f99d1fd2ba65d600c3334aea2ab072
sarberos <denismasters@mail.ru>
2024-09-14 12:23:43 +0300

    edit daily rew

--- src/widgets/Home/DailyRewards/DailyRewards.tsx
+++ src/widgets/Home/DailyRewards/DailyRewards.tsx
@@ -8,6 +8,8 @@ import { setBonusDay, setDailyRewardsStatus, setIsDailyReward, updateTotalCoins 
 import { TDayBoxProps } from '@shared/Home/types/dayBox'
 import { useTranslation } from 'react-i18next'
 import { EnumBonusStatus } from '@shared/Home/consts/bonusStatus.enum'
+import { SwiperSlide,Swiper } from 'swiper/react'
+import 'swiper/css'
 
 export default function({buttonActive}:{ buttonActive: boolean}){
     const {t} =useTranslation()
@@ -24,29 +26,89 @@ export default function({buttonActive}:{ buttonActive: boolean}){
             await claim_bonus();
             dispatch(setDailyRewardsStatus(EnumBonusStatus.WAIT))
     }
-    return(
-        <div className={s.daily_reward_wrap}>
-            <div className={s.reward_title}>
-                { t('dailyReward') }
-            </div>
-            <div className={s.box_slider}>
+    return (
+        
+      <div className={s.daily_reward_wrap}>
+        <div className={s.reward_title}>{t("dailyReward")}</div>
+        <div className={s.box_slider}>
+          <Swiper
+            spaceBetween={2}
+            initialSlide={2}
+            breakpoints={{  
+            320: {  
+                slidesPerView: 3,
+            },  
+            340: {  
+                slidesPerView: 3.25,
+            },  
+            350: {  
+                slidesPerView: 3.4,
+            },  
+            365: {  
+                slidesPerView: 3.5,
+            },  
+            377: {  
+                slidesPerView: 3.65,
+            },  
+            390: {  
+                slidesPerView: 3.75,
+            },  
+            405: {  
+                slidesPerView: 3.9,
+            },  
+            412: {  
+                slidesPerView: 4.0,
+            },  
+            425: {  
+                slidesPerView: 4,
+            },  
+            450: {  
+                slidesPerView: 4,
+            },  
+            550: {  
+                slidesPerView: 5,
+            },  
+            640: {  
+                slidesPerView: 6,
+            },  
+            768: {  
+                slidesPerView: 7, 
+            },
+            }}
+          >
+            {DAYBOXLIST.map((elem,index)=>(
+                <SwiperSlide>
+                    <DayBox currentDay={state.bonusDay} {...elem} key={index} />
+                </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        {/* <div className={s.box_slider}>
                 {DAYBOXLIST.map((elem,index)=>{
                     return elem.rewardDay >= state.bonusDay + 1   
                     ? <DayBox currentDay={state.bonusDay} {...elem} key={index} />   
                     : null  
                 })}
-            </div>
-            <div className={s.rewar_subtitle}>
+            </div> */}
+        <div className={s.rewar_subtitle}>
           {`${t("rewardsProfit")}: ${DAYBOXLIST.reduce((acc, item) => {
             return item.rewardDay <= state.bonusDay
               ? acc + item.rewardValue
               : acc;
           }, 0)}`}
         </div>
-            <div className={s.claim_btn}>
-                {buttonActive && <MainBtn  event={()=>onClaimBonus(state.bonusDay)} >{t('claim')}</MainBtn>}
-                {!buttonActive && <MainBtn  backColor={'#282828'} disabled={true} ><p style={{color:'#6C6C6C'}}>{t('claim')}</p></MainBtn>}
-            </div>
+        <div className={s.claim_btn}>
+          {buttonActive && (
+            <MainBtn event={() => onClaimBonus(state.bonusDay)}>
+              {t("claim")}
+            </MainBtn>
+          )}
+          {!buttonActive && (
+            <MainBtn backColor={"#282828"} disabled={true}>
+              <p style={{ color: "#6C6C6C" }}>{t("claim")}</p>
+            </MainBtn>
+          )}
         </div>
-    )
+      </div>
+    );
 }
