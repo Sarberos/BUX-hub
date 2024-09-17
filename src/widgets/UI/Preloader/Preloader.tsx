f99460da1964afc33c72fa238ad58a0470521fee
sarberos <denismasters@mail.ru>
2024-09-17 18:20:16 +0300

    new preloader

--- src/widgets/UI/Preloader/Preloader.tsx
+++ src/widgets/UI/Preloader/Preloader.tsx
@@ -1,13 +1,12 @@
 import s from './Preloader.module.scss'
-// import preloder_img from '@shared/UIComponents/assets/preloader.gif'
-import preloder_img from '@shared/UIComponents/assets/trasp_preloader.gif'
+import preloder_git from '@shared/UIComponents/assets/my_loader_gif.svg'
+import preloader_bg_img from '@shared/Wrap/assets/img/farm_page.png'
 
 export const Preloader=()=>{
     return(
         <div className={s.preloader_wrap}>
-            <div className={s.preloader_inner_wrap}>
-                    <img src={preloder_img} alt="" className={s.preloader_img} />
-            </div>
+            <img src={preloader_bg_img} alt="" className={s.preloader_bg_img} />
+            <img src={preloder_git} alt="" className={s.preloader_img} />
         </div>
     )
 }
