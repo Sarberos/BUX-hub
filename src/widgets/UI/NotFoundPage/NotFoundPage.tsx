4b3b39ab136a63bcee424360c68799f42ff3e740
sarberos <denismasters@mail.ru>
2024-11-12 22:02:34 +0300

    edit not_found page

--- src/widgets/UI/NotFoundPage/NotFoundPage.tsx
+++ src/widgets/UI/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,23 @@
 import { useNavigate } from "react-router"
 import s from './NotFoundPage.module.scss'
-import {Preloader} from "@widgets/UI/Preloader/Preloader.tsx";
+import Lottie from "react-lottie";
+import BUXloader from "@shared/UIComponents/assets/BUX_TXT_Loader.json"
+
 export default function NotFoundPage(){
     const navigate= useNavigate()
-
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: BUXloader,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      } }
     return(
-      <>
-        <Preloader backgroundImage={'src/shared/UIComponents/assets/not_found_bg.webp'} />
-          <button onClick={()=>navigate('/')} className={s.not_found_btn}>HOME</button>
-      </>
+      <div className={s.notfound_wrap}>
+        <div className={s.notfound_anim}>
+          <Lottie options={defaultOptions} height={210} width={210}/>
+        </div>
+        <button onClick={() => navigate('/')} className={s.not_found_btn}>HOME</button>
+      </div>
     )
 }
